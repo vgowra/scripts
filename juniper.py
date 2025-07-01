321beff63686320a7b440dac4302bf3f3e902864
vgowra <sairamgowra@gmail.com>
2025-07-01 17:19:57 -0400

    Update juniper.py

--- juniper.py
+++ juniper.py
@@ -1,15 +1,19 @@
 from netmiko import ConnectHandler
 import getpass
 
+#prompt for username and password
 username = input("Enter your Username: ")
 password = getpass.getpass("Enter your password: ")
 
+# Read bulk devices FQDNs/IPs from a file
 with open('devices.txt') as f:
     devices = [host.strip() for host in f.readlines()]
 
+# Read device configurations that are neede to be added/deleted/modified from a file
 with open('commands.txt') as f:
     commands = [cmd.strip() for cmd in f.readlines()]
 
+# Loop through each device and run command
 for host in devices:
     try:
         conn = ConnectHandler(
